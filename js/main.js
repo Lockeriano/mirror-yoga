530b45f4c0d54447e2704a397a683acade644cb6
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-09-22 12:03:51 +0200

    Slider complete but still got to making it responsive and fix JS code

--- js/main.js
+++ js/main.js
@@ -1,57 +1,42 @@
-$(function(){
-	$('.car-left').click(function(){
-		$('.client-1').css({
-					"opacity": "0.1",
-					"z-index": "300",
-					"left": "3%",
-					"border": "0",
-					"transform":"matrix(0.75, 0, 0, 0.75, 0, 0)"})
-		$('.client-2').css({
-					"opacity": "0.1",
-					"z-index": "300",
-					"left": "18%",
-					"border": "0",
-					"transform":"matrix(0.75, 0, 0, 0.75, 0, 0)"})
-		$('.client-3').css({
-					"opacity": "1",
-					"z-index": "500",
-					"left": "14%",
-					"border": "0",
-					"transform":"matrix(1, 0, 0, 1, 0, 0)"})
-	});
-});
+$(function() {
+	$(".car-left").click(function(){
+		$(".client-1").css({
+			"transition-duration": "1s",
+			"opacity": "0.1",
+			"z-index":"300",
+			"transform":"translate(-120px,0)"
+		})
+		$(".client-2").css({
+			"transition-duration": "1s",
+			"opacity": "0.1",
+			"z-index":"300",
+			"transform":"translate(285px, 0)"
+		})
+		$(".client-3").css({
+			"transition-duration": "1s",
+			"opacity": "1",
+			"z-index":"500",
+			"transform":"translate(-150px, 20px)"
+		})
+	})
+})
 
-$(function(){
-	$('.car-right').click(function(){
-		$('.client-1').css({
-					"opacity": "0.1",
-					"z-index": "300",
-					"left": "27%",
-					"display": "block",
-					"height": "auto",
-					"float": "left",
-					"width": "100%",
-					"border": "0",
-					"transform":"matrix(0.75, 0, 0, 0.75, 0, 0)"})
-		$('.client-2').css({
-					"opacity": "1",
-					"z-index": "500",
-					"left": "18%",
-					"display": "block",
-					"height": "auto",
-					"float": "left",
-					"width": "100%",
-					"border": "0",
-					"transform":"matrix(1, 0, 0, 1, 0, 0)"})
-		$('.client-3').css({
-					"opacity": "0.1",
-					"z-index": "300",
-					"left": "-8%",
-					"display": "block",
-					"height": "auto",
-					"float": "left",
-					"width": "100%",
-					"border": "0",
-					"transform":"matrix(0.75, 0, 0, 0.75, 0, 0)"})
-	});
-});
+$(function() {
+	$(".car-right").click(function(){
+		$(".client-1").css({
+			"opacity": "0.1",
+			"z-index":"300",
+			"transform":"translate(170px,0)"
+		})
+		$(".client-2").css({
+			"opacity": "1",
+			"z-index":"500",
+			"transform":"translate(140px, 20px)"
+		})
+		$(".client-3").css({
+			"opacity": "0.1",
+			"z-index":"300",
+			"transform":"translate(-285px, 0)"
+		})
+	})
+})
