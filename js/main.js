eb2b207caf30930313f44cff72a8c2eced995399
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-09-24 16:56:37 +0200

    Finished prices without proper hovers and bottom colors

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 $(function() {
 	$(".car-left").click(function(){
 		$(".client-1").css({
-			"transition-duration": "1s",
 			"opacity": "0.1",
 			"z-index":"300",
 			"transform":"translate(-120px,0)"
 		})
 		$(".client-2").css({
-			"transition-duration": "1s",
 			"opacity": "0.1",
 			"z-index":"300",
 			"transform":"translate(285px, 0)"
 		})
 		$(".client-3").css({
-			"transition-duration": "1s",
 			"opacity": "1",
 			"z-index":"500",
 			"transform":"translate(-150px, 20px)"
