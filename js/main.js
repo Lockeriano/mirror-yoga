5854bccf2737bb3a77d7d22e8e95a64cf80d3955
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-10-06 14:54:28 +0200

    Added nearly working carousel

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ $(function() {
 */
 
 var findLeft = document.getElementById("car-left");
+var findRight = document.getElementById("car-right");
 var frontImg = document.querySelector(".client-1");
 var leftImg = document.querySelector(".client-2");
 var rightImg = document.querySelector(".client-3");
@@ -72,7 +73,47 @@ findLeft.addEventListener("click", function (){
       if (leftImg.style.opacity = ("0.1")) {
         frontImg.style.opacity = ("1");
         frontImg.style.zIndex = ("500");
-        frontImg.style.transform = ("translate(20px,20px)");
+        frontImg.style.transform = ("translate(30px,20px)");
+        leftImg.style.opacity = ("0.1");
+        leftImg.style.zIndex = ("300");
+        leftImg.style.transform = ("translate(-10px,0px)");
+        rightImg.style.opacity = ("0.1");
+        rightImg.style.zIndex = ("300");
+        rightImg.style.transform = ("translate(0px,0px)");
+      }
+    });
+  });
+});
+
+findRight.addEventListener("click", function (){
+  if (frontImg.style.opacity = ("1")) {
+    frontImg.style.opacity = ("0.1");
+    frontImg.style.zIndex = ("300");
+    frontImg.style.transform = ("translate(-120px,0px)");
+    leftImg.style.opacity = ("0.1");
+    leftImg.style.zIndex = ("300");
+    leftImg.style.transform = ("translate(285px,0px)");
+    rightImg.style.opacity = ("1");
+    rightImg.style.zIndex = ("500");
+    rightImg.style.transform = ("translate(-150px,20px)");
+  }
+  findRight.addEventListener("click", function (){
+    if (rightImg.style.opacity = ("1")) {
+      frontImg.style.opacity = ("0.1");
+      frontImg.style.zIndex = ("300");
+      frontImg.style.transform = ("translate(175px,0px)");
+      leftImg.style.opacity = ("1");
+      leftImg.style.zIndex = ("500");
+      leftImg.style.transform = ("translate(130px,20px)");
+      rightImg.style.opacity = ("0.1");
+      rightImg.style.zIndex = ("300");
+      rightImg.style.transform = ("translate(-295px,0px)");
+    }
+    findRight.addEventListener("click", function (){
+      if (leftImg.style.opacity = ("1")) {
+        frontImg.style.opacity = ("1");
+        frontImg.style.zIndex = ("500");
+        frontImg.style.transform = ("translate(30px,20px)");
         leftImg.style.opacity = ("0.1");
         leftImg.style.zIndex = ("300");
         leftImg.style.transform = ("translate(-10px,0px)");
